385abbdb94d8d54b9c0339d49cc47f473355bf3c
OR0241640 <ANDREW.DAVENPORT@odhsoha.oregon.gov>
2025-03-19 08:40:03 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -60,5 +60,5 @@ st.text(smoothiefroot_response)
 # New section to display smoothiefroot nutrition information
 import requests
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-#st.text(smoothiefroot_response.json())
-sf_df = st.dataframe(data==smoothiefroot_response.json(), use_container_width=True)
+st.text(smoothiefroot_response.json())
+#sf_df = st.dataframe(data==smoothiefroot_response.json(), use_container_width=True)
